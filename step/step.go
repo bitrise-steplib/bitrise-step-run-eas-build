c6f80142572899f35f3aabbdbec84b19e669f61f
godrei <krisztian.godrei@bitrise.io>
2022-02-01 17:07:24 +0100

    Introduce ExpoClient interface

--- step/step.go
+++ step/step.go
@@ -1,21 +1,35 @@
 package step
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/bitrise-io/go-steputils/v2/stepconf"
 )
 
 // Input ...
 type Input struct {
-	User     stepconf.Secret `env:"user,required"`
-	Password stepconf.Secret `env:"password,required"`
+	User      stepconf.Secret `env:"user,required"`
+	Password  stepconf.Secret `env:"password,required"`
+	Platforms string          `env:"platforms,required"`
 }
 
 // Config ...
-type Config Input
+type Config struct {
+	User      stepconf.Secret
+	Password  stepconf.Secret
+	Platforms []string
+}
+
+type ExpoClient interface {
+	Login(user, password stepconf.Secret) error
+	Build(platforms []string, options ...string) error
+}
 
 // RunEASBuild ...
 type RunEASBuild struct {
 	inputParser stepconf.InputParser
+	expoClient  ExpoClient
 }
 
 // NewRunEASBuild ...
@@ -30,8 +44,25 @@ func (s RunEASBuild) ProcessConfig() (Config, error) {
 	if err != nil {
 		return Config{}, err
 	}
-	stepconf.Print(input) // TODO: log.Infof(stepconf.toString(input))
-	return Config(input), nil
+	stepconf.Print(input)
+
+	var platforms []string
+	split := strings.Split(input.Platforms, "\n")
+	for _, e := range split {
+		e = strings.TrimSpace(e)
+		if len(e) > 0 {
+			platforms = append(platforms, e)
+		}
+	}
+	if len(platforms) == 0 {
+		return Config{}, errors.New("no platform specified")
+	}
+
+	return Config{
+		User:      input.User,
+		Password:  input.Password,
+		Platforms: platforms,
+	}, nil
 }
 
 // InstallDeps ...
@@ -45,6 +76,14 @@ type Result struct {
 
 // Run ...
 func (s RunEASBuild) Run(cfg Config) (Result, error) {
+	if err := s.expoClient.Login(cfg.User, cfg.Password); err != nil {
+		return Result{}, err
+	}
+
+	if err := s.expoClient.Build(cfg.Platforms); err != nil {
+		return Result{}, err
+	}
+
 	return Result{}, nil
 }
 
