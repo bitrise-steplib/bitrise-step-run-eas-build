8180a0b327f43e54c67042f46526e65db81e948a
godrei <krisztian.godrei@bitrise.io>
2022-02-02 15:57:30 +0100

    Add tests

--- go.mod
+++ go.mod
@@ -5,6 +5,13 @@ go 1.17
 require (
 	github.com/bitrise-io/go-steputils/v2 v2.0.0-alpha.1
 	github.com/bitrise-io/go-utils/v2 v2.0.0-alpha.2
+	github.com/stretchr/testify v1.7.0
 )
 
-require github.com/bitrise-io/go-utils v1.0.1 // indirect
+require (
+	github.com/bitrise-io/go-utils v1.0.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
--- step/step.go
+++ step/step.go
@@ -2,6 +2,7 @@ package step
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/bitrise-io/go-steputils/v2/stepconf"
 	"github.com/bitrise-io/go-utils/v2/log"
@@ -52,9 +53,44 @@ func (s EASBuilder) Run(input Input) error {
 	s.logger.Println()
 	s.logger.TInfof("Running EAS build")
 
+	start := time.Now()
+
 	if err := client.Build(input.Platform); err != nil {
 		return fmt.Errorf("running eas build failed: %w", err)
 	}
 
+	s.logger.TDonef("Finished in: %s", runtime(time.Since(start)))
+
 	return nil
+}
+
+func runtime(d time.Duration) string {
+	const minToSec = 60
+	const hToMin = 60
+
+	elapsed := int(d / time.Second)
+
+	min := elapsed / minToSec
+	sec := elapsed - (min * minToSec)
+
+	h := min / hToMin
+	min -= h * hToMin
+
+	s := ""
+	if h > 0 {
+		s += fmt.Sprintf("%dh", h)
+	}
+	if min > 0 {
+		if len(s) > 0 {
+			s += " "
+		}
+		s += fmt.Sprintf("%dm", min)
+	}
+	if sec > 0 {
+		if len(s) > 0 {
+			s += " "
+		}
+		s += fmt.Sprintf("%ds", sec)
+	}
+	return s
 }
